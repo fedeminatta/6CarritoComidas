86741a55812090e1d017a51299bbd6b2784ed8c8
fedeminatta <minattafederico@gmail.com>
2022-10-31 20:37:40 -0300

    cambio

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 let articulosCarrito = [];
 const btnSun = document.querySelector('#sun');
 const rootStyle = document.documentElement.style;
