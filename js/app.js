d147b6cc04a9800ab13679ebd67ad6d8a2863759
fedeminatta <minattafederico@gmail.com>
2023-04-25 20:40:45 -0300

    s

--- js/app.js
+++ js/app.js
@@ -10,85 +10,85 @@ const cardImg = document.querySelector('.card img');
 cargarEventListeners();
 
 function cargarEventListeners() {
-  // Dispara cuando se presiona "Agregar Carrito"
-  listaCursos.addEventListener('click', agregarCurso);
+	// Dispara cuando se presiona "Agregar Carrito"
+	listaCursos.addEventListener('click', agregarCurso);
 
-  // Cuando se elimina un curso del carrito
-  carrito.addEventListener('click', eliminarCurso);
+	// Cuando se elimina un curso del carrito
+	carrito.addEventListener('click', eliminarCurso);
 
-  // Muestra los cursos del localStorage
-  document.addEventListener('DOMContentLoaded', () => {
-    articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+	// Muestra los cursos del localStorage
+	document.addEventListener('DOMContentLoaded', () => {
+		articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-    carritoHTML();
-  });
+		carritoHTML();
+	});
 }
 
 // Funciones
 // Función que añade el curso al carrito
 function agregarCurso(e) {
-  e.preventDefault();
-  // Delegation para agregar-carrito
-  if (e.target.classList.contains('agregar-carrito')) {
-    const curso = e.target.parentElement.parentElement;
-    // Enviamos el curso seleccionado para tomar sus datos
-    leerDatosCurso(curso);
-  }
+	e.preventDefault();
+	// Delegation para agregar-carrito
+	if (e.target.classList.contains('agregar-carrito')) {
+		const curso = e.target.parentElement.parentElement;
+		// Enviamos el curso seleccionado para tomar sus datos
+		leerDatosCurso(curso);
+	}
 }
 
 // Lee los datos del curso
 function leerDatosCurso(curso) {
-  const infoCurso = {
-    imagen: curso.querySelector('img').src,
-    titulo: curso.querySelector('h4').textContent,
-    precio: curso.querySelector('.precio span').textContent,
-    id: curso.querySelector('a').getAttribute('data-id'),
-    cantidad: 1,
-  };
+	const infoCurso = {
+		imagen: curso.querySelector('img').src,
+		titulo: curso.querySelector('h4').textContent,
+		precio: curso.querySelector('.precio span').textContent,
+		id: curso.querySelector('a').getAttribute('data-id'),
+		cantidad: 1,
+	};
 
-  if (articulosCarrito.some((curso) => curso.id === infoCurso.id)) {
-    const cursos = articulosCarrito.map((curso) => {
-      if (curso.id === infoCurso.id) {
-        curso.cantidad++;
-        return curso;
-      } else {
-        return curso;
-      }
-    });
-    articulosCarrito = [...cursos];
-  } else {
-    articulosCarrito = [...articulosCarrito, infoCurso];
-  }
+	if (articulosCarrito.some((curso) => curso.id === infoCurso.id)) {
+		const cursos = articulosCarrito.map((curso) => {
+			if (curso.id === infoCurso.id) {
+				curso.cantidad++;
+				return curso;
+			} else {
+				return curso;
+			}
+		});
+		articulosCarrito = [...cursos];
+	} else {
+		articulosCarrito = [...articulosCarrito, infoCurso];
+	}
 
-  // console.log(articulosCarrito)
+	// console.log(articulosCarrito)
 
-  // console.log(articulosCarrito)
-  carritoHTML();
+	// console.log(articulosCarrito)
+	carritoHTML();
 }
 
 // Elimina el curso del carrito en el DOM
 function eliminarCurso(e) {
-  e.preventDefault();
-  if (e.target.classList.contains('borrar-curso')) {
-    // e.target.parentElement.parentElement.remove();
-    const cursoId = e.target.getAttribute('data-id');
+	e.preventDefault();
+	if (e.target.classList.contains('borrar-curso')) {
+		// e.target.parentElement.parentElement.remove();
+		const cursoId = e.target.getAttribute('data-id');
 
-    // Eliminar del arreglo del carrito
-    articulosCarrito = articulosCarrito.filter((curso) => curso.id !== cursoId);
+		// Eliminar del arreglo del carrito
+		articulosCarrito = articulosCarrito.filter((curso) => curso.id !== cursoId);
 
-    carritoHTML();
-  }
+		carritoHTML();
+	}
 }
 
 // Muestra el curso seleccionado en el Carrito
 function carritoHTML() {
-  // limpia el html
-  vaciarCarrito();
+	// limpia el html
+	vaciarCarrito();
 
-  // recorre el carrito y genera el html
-  articulosCarrito.forEach((curso) => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
+	// recorre el carrito y genera el html
+	articulosCarrito.forEach((curso) => {
+		const row = document.createElement('tr');
+		row.innerHTML = `
                <td>  
                     <img src="${curso.imagen}" width=100>
                </td>
@@ -99,27 +99,27 @@ function carritoHTML() {
                     <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
                </td>
           `;
-    // Agrega el HTML del carrito en el tbody
-    contenedorCarrito.appendChild(row);
-  });
+		// Agrega el HTML del carrito en el tbody
+		contenedorCarrito.appendChild(row);
+	});
 
-  // Agregar el carrito de compras al Storage
-  sincronizarStorage();
+	// Agregar el carrito de compras al Storage
+	sincronizarStorage();
 }
 
 function sincronizarStorage() {
-  localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
+	localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
 }
 
 // Elimina los cursos del carrito en el DOM
 function vaciarCarrito() {
-  // forma lenta
-  // contenedorCarrito.innerHTML = '';
+	// forma lenta
+	// contenedorCarrito.innerHTML = '';
 
-  // forma rapida (recomendada)
-  while (contenedorCarrito.firstChild) {
-    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
-  }
+	// forma rapida (recomendada)
+	while (contenedorCarrito.firstChild) {
+		contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+	}
 }
 
 // eventos del modo oscuro
@@ -127,21 +127,21 @@ btnSun.addEventListener('click', darkMode);
 
 //
 function darkMode() {
-  if (btnSun.classList.contains('fa-sun')) {
-    btnSun.classList.remove('fa-sun');
-    btnSun.classList.add('fa-moon');
-    rootStyle.setProperty('--bg-primary', '#191919');
-    rootStyle.setProperty('--bg-secundary', '#202020');
-    rootStyle.setProperty('--text', '#fff');
-    rootStyle.setProperty('--shadow', '#131313');
-    rootStyle.setProperty('--bg-card', '#191919');
-  } else if (btnSun.classList.contains('fa-moon')) {
-    btnSun.classList.remove('fa-moon');
-    btnSun.classList.add('fa-sun');
-    rootStyle.setProperty('--bg-primary', '#fff');
-    rootStyle.setProperty('--bg-secundary', '#f8f8f8');
-    rootStyle.setProperty('--text', '#000');
-    rootStyle.setProperty('--shadow', '#cacaca');
-    rootStyle.setProperty('--bg-card', '#fff');
-  }
+	if (btnSun.classList.contains('fa-moon')) {
+		btnSun.classList.remove('fa-moon');
+		btnSun.classList.add('fa-sun');
+		rootStyle.setProperty('--bg-primary', '#191919');
+		rootStyle.setProperty('--bg-secundary', '#202020');
+		rootStyle.setProperty('--text', '#fff');
+		rootStyle.setProperty('--shadow', '#131313');
+		rootStyle.setProperty('--bg-card', '#191919');
+	} else if (btnSun.classList.contains('fa-sun')) {
+		btnSun.classList.remove('fa-sun');
+		btnSun.classList.add('fa-moon');
+		rootStyle.setProperty('--bg-primary', '#fff');
+		rootStyle.setProperty('--bg-secundary', '#f8f8f8');
+		rootStyle.setProperty('--text', '#000');
+		rootStyle.setProperty('--shadow', '#cacaca');
+		rootStyle.setProperty('--bg-card', '#fff');
+	}
 }
